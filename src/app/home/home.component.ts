45e811d4d3701f25b7cf48077130e0c5e33e5167
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-02-25 23:25:37 +0000

    added HttpClient Module to connect to reqres api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,16 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 
-  h1Style: boolean = false;
+   h1Style: boolean = false;
+   users: Object;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    })
   }
 
   firstClick() {
